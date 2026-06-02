a9eade0844fe578b5664ed4d7f27121bd5d59461
Joao <86856724+JPereiraLab@users.noreply.github.com>
2026-06-02 23:00:44 +0100

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,6 @@
+resource "azuread_group" "security_group" {
+  display_name     = var.group_name
+  description      = var.group_description
+  security_enabled = true
+  mail_enabled     = false
+}
