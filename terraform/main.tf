a7b3640af9cd03bb1e7a606018b505a0061bdc80
Joao <86856724+JPereiraLab@users.noreply.github.com>
2026-06-03 15:35:07 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,10 @@
+resource "azuread_group" "security_group" {
+  display_name     = var.group_name
+  description      = var.group_description
+  security_enabled = true
+  mail_enabled     = false
+}
+
 resource "azuread_group" "second_group" {
   display_name     = "Second-Test-Group"
   description      = "Added via the GitOps PR flow"
